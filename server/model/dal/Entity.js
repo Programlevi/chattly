8453e80b2cb0cbd7a8f9bfb73e8786a4dc62cd7b
programLevi <programLevi@gmail.com>
2020-06-10 14:36:05 +0100

    Setup data access layer

---
+++ server/model/dal/Entity.js
@@ -0,0 +1,114 @@
+const { sql } = require('slonik');
+const pool = require('../pool');
+const isObject = require('../../utils/isObject');
+
+class Entity {
+  constructor(tableName, columns) {
+    this.tableName = tableName;
+    this.columns = columns;
+  }
+
+  async find(condition = true) {
+    if (!isObject(condition) && condition !== true)
+      throw new Error(
+        'Invalid input: condition must be object or undefined and cannot be null'
+      );
+
+    if (typeof condition !== 'boolean')
+      condition = this._generateColumnValuePairs(condition, sql` AND `);
+
+    const res = await pool.query(
+      sql`SELECT * FROM ${sql.identifier([this.tableName])} WHERE ${condition}`
+    );
+    return res.rows;
+  }
+
+  async add(records) {
+    if (!Array.isArray(records))
+      throw new Error('Invalid input: must be an array of records');
+
+    const columns = this._generateColumns();
+    const values = this._generateValues(records);
+
+    const res = await pool.query(
+      sql`INSERT INTO ${sql.identifier([
+        this.tableName
+      ])} ${columns} VALUES ${values} RETURNING *`
+    );
+
+    return res.rows;
+  }
+
+  async update(data, condition = true) {
+    if (!isObject(data)) throw new Error('Invalid input: data must be object');
+    if (!isObject(condition) && condition !== true)
+      throw new Error(
+        'Invalid input: condition must be object or undefined and cannot be null'
+      );
+
+    if (typeof condition !== 'boolean')
+      condition = this._generateColumnValuePairs(condition, sql` AND `);
+
+    const updates = this._generateColumnValuePairs(data, sql`, `);
+
+    const res = await pool.query(
+      sql`UPDATE ${sql.identifier([
+        this.tableName
+      ])} SET ${updates} WHERE ${condition} RETURNING *`
+    );
+
+    console.log(res.rows);
+    return res.rows;
+  }
+
+  async delete(condition = true) {
+    if (!isObject(condition) && condition !== true) {
+      throw new Error(
+        'Invalid input: condition must be object or undefined and cannot be null'
+      );
+    }
+    if (typeof condition !== 'boolean') {
+      condition = this._generateColumnValuePairs(condition, sql` AND `);
+    }
+    const res = await pool.query(
+      sql`DELETE FROM ${sql.identifier([
+        this.tableName
+      ])} WHERE ${condition} RETURNING *`
+    );
+
+    return res.rows;
+  }
+
+  _generateColumnValuePairs(obj, glue) {
+    const sqlArray = Object.keys(obj).map(
+      col => sql`${sql.identifier([col])} = ${obj[col]}`
+    );
+
+    return sql.join(sqlArray, glue);
+  }
+
+  _generateColumns() {
+    return sql`(${sql.join(
+      this.columns.map(col => sql.identifier([col])),
+      sql`, `
+    )})`;
+  }
+
+  _generateValues(records) {
+    const sqlArray = records.map(
+      record =>
+        sql`(${sql.join(
+          this.columns.map(col => {
+            if (!record[col]) {
+              return sql`DEFAULT`;
+            }
+            return record[col];
+          }),
+          sql`, `
+        )})`
+    );
+    return sql.join(sqlArray, sql`, `);
+  }
+}
+
+module.exports = Entity;
