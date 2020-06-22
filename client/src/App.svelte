397466fe31bb0bfd88024d873c661b4952b58bcb
programLevi <programLevi@gmail.com>
2020-06-22 18:36:17 +0100

    Signup page done.

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -47,5 +47,81 @@
 
   :global(a) {
     color: var(--main-color);
+    text-decoration: none;
+  }
+
+  :global(.auth-page) {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-height: 100vh;
+  }
+
+  :global(.auth-form) {
+    background-color: #fff;
+    width: 55rem;
+    padding: 5rem 7rem;
+    box-shadow: rgba(0, 0, 0, 0.059) 0px 2.5rem 8rem 2rem;
+  }
+
+  :global(.form-heading) {
+    color: var(--main-color);
+    font-weight: 500;
+    font-size: 2.25rem;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    margin-bottom: 3.5rem;
+  }
+
+  :global(.form-group:not(:last-child)) {
+    margin-bottom: 2.5rem;
+  }
+
+  :global(.form-group label) {
+    display: block;
+    margin-bottom: 0.75rem;
+  }
+
+  :global(.form-group input) {
+    display: block;
+    color: inherit;
+    padding: 1.25rem 1.75rem;
+    width: 100%;
+    background-color: var(--main-bg-color);
+    border: none;
+    border-bottom: 3px solid transparent;
+    border-radius: 2px;
+    transition: all 0.3s;
+  }
+
+  :global(.form-group input:focus) {
+    outline: none;
+    border-bottom-color: var(--main-color);
+  }
+
+  :global(.form-group button) {
+    color: #fff;
+    padding: 1.4rem 3rem;
+    font-weight: 400;
+    font-size: 1.6rem;
+    font-family: inherit;
+    border: none;
+    border-radius: 2px;
+    text-transform: uppercase;
+    background-color: var(--main-color);
+    cursor: pointer;
+    transition: all 0.3s;
+  }
+
+  :global(.form-group button:hover, .form-group button:active) {
+    box-shadow: rgba(0, 0, 0, 0.149) 0px 0.5rem 1rem;
+  }
+
+  :global(.form-group button:focus) {
+    outline: none;
+  }
+
+  :global(.form-group .alternate-auth) {
+    font-size: 1.2rem;
   }
 </style>
--- client/src/pages/signup.svelte
+++ client/src/pages/signup.svelte
@@ -1,1 +1,72 @@
-<h1>Signup</h1>
+<script>
+  import { url } from '@sveltech/routify';
+  let signupDetails = {
+    userName: '',
+    email: '',
+    password: '',
+    passwordConfirm: ''
+  };
+</script>
+
+<main class="auth-page">
+  <form class="auth-form" on:submit|preventDefault>
+    <h1 class="form-heading">Create a new account</h1>
+
+    <div class="form-group">
+      <label for="userName">Username</label>
+      <input
+        type="text"
+        id="userName"
+        name="userName"
+        placeholder="bond007"
+        autocomplete="off"
+        bind:value={signupDetails.userName}
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="email">Email Address</label>
+      <input
+        type="eamil"
+        id="email"
+        name="email"
+        placeholder="bassey08@gmail.com"
+        autocomplete="off"
+        bind:value={signupDetails.email}
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input
+        type="password"
+        id="password"
+        name="password"
+        placeholder="Enter your password"
+        bind:value={signupDetails.password}
+      />
+    </div>
+
+    <div class="form-group">
+      <label for="passwordConfirm">Confirm Password</label>
+      <input
+        type="password"
+        id="passwordConfirm"
+        name="passwordConfirm"
+        placeholder="Retype your password"
+        bind:value={signupDetails.passwordConfirm}
+      />
+    </div>
+
+    <div class="form-group">
+      <button>Sign Up</button>
+    </div>
+
+    <div class="form-group">
+      <p class="alternate-auth">
+        Already have an account?
+        <a href={$url('../login')}>Log in here</a>
+      </p>
+    </div>
+  </form>
+</main>
