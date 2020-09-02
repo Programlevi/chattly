0e72010770eea491c02d0ce8fb9806ebf87ebd59
programLevi <programLevi@gmail.com>
2020-09-02 13:06:32 +0100

    Fixed cookie issue

--- server/utils/createJwtCookie.js
+++ server/utils/createJwtCookie.js
@@ -4,6 +4,7 @@ module.exports = (res, req, token) => {
     expires: new Date(
       Date.now() + process.env.JWT_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https'
+    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    sameSite: 'none'
   });
 };
